ccc7b8e5bd68bbf80a9a66bc4e22f6eef57e1488
Andre Martins <andre.t.martins@gmail.com>
2018-06-17 22:31:05 +0100

    Implemented dependency parts.

--- parser/dependency_options.py
+++ parser/dependency_options.py
@@ -4,6 +4,8 @@ class DependencyOptions(Options):
     '''Options for the dependency parser.'''
     def __init__(self, parser):
         Options.__init__(self, parser)
+
+        # Token options.
         parser.add_argument('--form_cutoff', type=int, default=0,
                             help="""Ignore word forms whose frequency is less
                             than this.""")
@@ -24,8 +26,86 @@ class DependencyOptions(Options):
                             default=0, const=1,
                             help='Distinguish upper/lower case of word forms.')
 
+        # Parser options.
+        parser.add_argument('--model_type', type=str, default='standard',
+                            help="""Model type. This a string formed by the one
+                            or several of the following pieces:
+                            af enables arc-factored parts (required),
+                            +cs enables consecutive sibling parts,
+                            +gp enables grandparent parts,
+                            +gs enables grandsibling parts,
+                            +ts enables trisibling parts,
+                            +as enables arbitrary sibling parts,
+                            +hb enables head bigram parts,
+                            +gs enables grand-sibling (third-order) parts,
+                            +ts enables tri-sibling (third-order) parts.
+                            The following alias are predefined:
+                            basic is af,
+                            standard is af+cs+gp,
+                            full is af+cs+gp+as+hb+gs+ts.""")
+        parser.add_argument('--unlabeled', action='store_const',
+                            default=0, const=1,
+                            help="""Make the parser output just the backbone
+                            dependencies.""")
+        parser.add_argument('--projective', action='store_const',
+                            default=0, const=1,
+                            help="""Force the parser output single-rooted
+                            projective trees.""")
+        parser.add_argument('--prune_relations', type=int, default=1,
+                            help="""1 for pruning the set of possible relations
+                            taking into account the labels that have occured for
+                            each pair of POS tags in the training data.""")
+        parser.add_argument('--prune_distances', type=int, default=1,
+                            help="""1 for pruning the set of possible left/right
+                            distances taking into account the distances that
+                            have occured for each pair of POS tags in the
+                            training data.""")
+        parser.add_argument('--prune_basic', type=int, default=1,
+                            help="""1 for using a basic pruner from a
+                            probabilistic first-order model.""")
+        parser.add_argument('--pruner_posterior_threshold', type=float,
+                            default=0.0001,
+                            help="""Posterior probability threshold for an arc
+                            to be pruned, in basic pruning. For each  word m,
+                            if P(h,m) < pruner_posterior_threshold * P(h',m),
+                            where h' is the best scored head, then (h,m) will be
+                            pruned out.""")
+        parser.add_argument('--pruner_max_heads', type=int,
+                            default=10,
+                            help="""Maximum number of possible head words for a
+                            given word, in basic pruning.""")
+
+        """
+// Options for pruner training.
+// TODO: implement these options.
+DEFINE_string(pruner_train_algorithm, "crf_mira",
+              "Training algorithm for the pruner. Options are perceptron, mira, "
+              "svm_mira, crf_mira, svm_sgd, crf_sgd.");
+DEFINE_bool(pruner_only_supported_features, true,
+            "True for the pruner to use supported features only (should be true"
+            "for CRFs).");
+DEFINE_bool(pruner_use_averaging, true,
+            "True for the pruner to average the weight vector at the end of"
+            "training.");
+DEFINE_int32(pruner_train_epochs, 10,
+             "Number of training epochs for the pruner.");
+DEFINE_double(pruner_train_regularization_constant, 0.001,
+              "Regularization parameter C for the pruner.");
+DEFINE_bool(pruner_labeled, false,
+            "True if pruner is a labeled parser. Currently, must be set to false.");
+DEFINE_double(pruner_train_initial_learning_rate, 0.01,
+              "Initial learning rate of pruner (for SGD only).");
+DEFINE_string(pruner_train_learning_rate_schedule, "invsqrt",
+              "Learning rate annealing schedule of pruner (for SGD only). "
+              "Options are fixed, lecun, invsqrt, inv.");
+DEFINE_bool(pruner_large_feature_set, false,
+            "True for using a large feature set in the pruner.");
+        """
+
+
     def parse_args(self, args):
         Options.parse_args(self, args)
+
         self.form_cutoff = args['form_cutoff']
         self.lemma_cutoff = args['lemma_cutoff']
         self.tag_cutoff = args['tag_cutoff']
@@ -33,3 +113,20 @@ class DependencyOptions(Options):
         self.prefix_length = args['prefix_length']
         self.suffix_length = args['suffix_length']
         self.form_case_sensitive = bool(args['form_case_sensitive'])
+
+        self.model_type = args['model_type']
+        self.unlabeled = bool(args['unlabeled'])
+        self.projective = bool(args['projective'])
+        self.prune_relations = bool(args['prune_relations'])
+        self.prune_distances = bool(args['prune_distances'])
+        self.prune_basic = bool(args['prune_basic'])
+        self.pruner_posterior_threshold = args['pruner_posterior_threshold']
+        self.pruner_max_heads = args['pruner_max_heads']
+
+        if self.model_type == 'basic':
+            self.model_type = 'af'
+        elif self.model_type == 'standard':
+            self.model_type = 'af+cs+gp'
+        elif self.model_type == 'full':
+            self.model_type = 'af+cs+gp+as+hb+gs+ts'
+
--- parser/turbo_parser.py
+++ parser/turbo_parser.py
@@ -5,6 +5,7 @@ from parser.dependency_writer import DependencyWriter
 from parser.dependency_decoder import DependencyDecoder
 from parser.dependency_dictionary import DependencyDictionary
 from parser.token_dictionary import TokenDictionary
+from parser.dependency_parts import DependencyParts
 import logging
 
 class TurboParser(StructuredClassifier):
@@ -19,6 +20,100 @@ class TurboParser(StructuredClassifier):
         self.parameters = None
         self.token_dictionary.initialize(self.reader)
         self.dictionary.create_relation_dictionary(self.reader)
+
+    def make_parts(self, instance):
+        return self.make_parts_basic(instance, add_relation_parts=False)
+
+    def make_parts_basic(self, instance, add_relation_parts=True):
+        parts = DependencyParts()
+        if add_relation_parts and not self.options.prune_relations:
+            allowed_relations = range(len(
+                self.dictionary.get_relation_alphabet()))
+
+        for h in range(len(instance)):
+            for m in range(1, len(instance)):
+                if h == m:
+                    continue
+                if add_relation_parts:
+                    # If no unlabeled arc is there, just skip it.
+                    # This happens if that arc was pruned out.
+                    if 0 > parts.find_arc(h, m):
+                        continue
+                else:
+                    if h and self.options.prune_distances:
+                        modifier_tag = instance.get_tag(m)
+                        head_tag = instance.get_tag(h)
+                        if h < m:
+                            # Right attachment.
+                            if m - h > \
+                               self.dictionary.get_maximum_right_distance(
+                                   modifier_tag, head_tag):
+                                continue
+                        else:
+                            # Left attachment.
+                            if h - m > \
+                               self.dictionary.get_maximum_left_distance(
+                                   modifier_tag, head_tag):
+                                continue
+                if self.options.prune_relations:
+                    modifier_tag = instance.get_tag(m)
+                    head_tag = instance.get_tag(h)
+                    allowed_labels = []
+                    allowed_labels = self.dictionary.get_existing_relations(
+                        modifier_tag, head_tag)
+                    if not add_relation_parts and not allowed_relations:
+                        continue
+
+                # Add parts for labeled/unlabeled arcs.
+                if add_relation_parts:
+                    # If there is no allowed relation for this arc, but the
+                    # unlabeled arc was added, then it was forced to be present
+                    # to maintain connectivity of the graph. In that case (which
+                    # should be pretty rare) consider all the possible
+                    # relations.
+                    if not allowed_relations:
+                        allowed_relations = range(len(
+                            self.dictionary.get_relation_alphabet()))
+                    for l in allowed_relations:
+                        part = parts.create_part_labeled_arc(h, m, l)
+                        parts.append(part)
+                        if make_gold:
+                            if instance.get_head(m) == h and \
+                               instance.get_relation(m) == l:
+                                gold_outputs.append(1.)
+                            else:
+                                gold_outputs.append(0.)
+                else:
+                    part = parts.create_part_arc(h, m)
+                    parts.append(part)
+                    if make_gold:
+                        if instance.get_head(m) == h:
+                            gold_outputs.append(1.)
+                        else:
+                            gold_outputs.append(0.)
+
+        # When adding unlabeled arcs, make sure the graph stays connected.
+        # Otherwise, enforce connectedness by adding some extra arcs
+        # that connect words to the root.
+        # NOTE: if --projective, enforcing connectedness is not enough,
+        # so we add arcs of the form m-1 -> m to make sure the sentence
+        # has a projective parse.
+        if not add_relation_parts:
+            arcs = parts[num_parts_initial:]
+            inserted_arcs = self.enforce_well_formed_graph(instance, arcs)
+            for h, m in inserted_arcs:
+                part = parts.create_part_arc(h, m)
+                parts.append(part)
+                if make_gold:
+                    if instance.get_head(m) == h:
+                        gold_outputs.append(1.)
+                    else:
+                        gold_outputs.append(0.)
+            parts.set_offset_arc(num_parts_initial,
+                                 len(parts) - num_parts_initial)
+        else:
+            parts.set_offset_labeled_arc(num_parts_initial,
+                                 len(parts) - num_parts_initial)
 
 def main():
     '''Main function for the dependency parser.'''
