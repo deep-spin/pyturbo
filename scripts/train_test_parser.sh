f82f03aea681a1ce08c0127efb678487ab77fbab
Andre Martins <andre.t.martins@gmail.com>
2018-07-09 23:22:19 +0100

    Define HOME in script.

--- scripts/train_test_parser.sh
+++ scripts/train_test_parser.sh
@@ -1,17 +1,19 @@
 cd ..
 
+HOME=/Users/afm
+
 python3 parser/turbo_parser.py \
         --train \
         --unlabeled \
-        --training_path /home/afm/projects/TurboParser/data/pt-ud-normalized/pt-ud-normalized_train.conll \
-        --training_epochs 10 \
+        --training_path $HOME/projects/TurboParser/data/pt-ud-normalized/pt-ud-normalized_train.conll \
+        --training_epochs 1 \
         --model_path model.pkl
 
 python3 parser/turbo_parser.py \
         --test \
         --evaluate \
-        --test_path /home/afm/projects/TurboParser/data/pt-ud-normalized/pt-ud-normalized_test.conll \
+        --test_path $HOME/projects/TurboParser/data/pt-ud-normalized/pt-ud-normalized_test.conll \
         --model_path model.pkl \
         --output_path out.conll
 
-perl scripts/eval.pl -b -q -g /home/afm/projects/TurboParser/data/pt-ud-normalized/pt-ud-normalized_test.conll -s out.conll | tail -5
+perl scripts/eval.pl -b -q -g $HOME/projects/TurboParser/data/pt-ud-normalized/pt-ud-normalized_test.conll -s out.conll | tail -5
