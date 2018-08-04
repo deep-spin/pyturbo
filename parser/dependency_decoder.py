c746035df42d9f293b104fe65e3a7391d555f210
Erick Fonseca <erickrfonseca@gmail.com>
2018-08-04 12:10:44 +0100

    started implementing next sibling decoder

--- parser/dependency_decoder.py
+++ parser/dependency_decoder.py
@@ -1,6 +1,8 @@
 from classifier.structured_decoder import StructuredDecoder
 from parser.dependency_instance import DependencyInstance
-from parser.dependency_parts import DependencyPartArc, DependencyPartLabeledArc
+from parser.dependency_parts import DependencyPartArc, \
+    DependencyPartLabeledArc, DependencyPartConsecutiveSibling, \
+    DependencyParts
 import ad3.factor_graph as fg
 from ad3.extensions import PFactorTree
 import numpy as np
@@ -10,6 +12,15 @@ class DependencyDecoder(StructuredDecoder):
         StructuredDecoder.__init__(self)
 
     def decode(self, instance, parts, scores):
+        """
+        Decode the scores to the dependency parts under the necessary
+        contraints, yielding a valid dependency tree.
+
+        :param instance: DependencyInstance
+        :type parts: DependencyParts
+        :param scores: array or tensor with scores for each part
+        :return:
+        """
         length = len(instance)
         offset_arcs, num_arcs = parts.get_offset(DependencyPartArc)
 
@@ -20,11 +31,19 @@ class DependencyDecoder(StructuredDecoder):
         variables = []
         for r in range(offset_arcs, offset_arcs + num_arcs):
             arc_indices.append((parts[r].head, parts[r].modifier))
-            arc = graph.create_binary_variable()
-            arc.set_log_potential(scores[r])
-            variables.append(arc)
-        #if length == 32:
-        #    import pdb; pdb.set_trace()
+            arc_variable = graph.create_binary_variable()
+            arc_variable.set_log_potential(scores[r])
+            variables.append(arc_variable)
+
+        # add constraints for consecutive siblings
+        offset_arcs, num_arcs = parts.get_offset(
+            DependencyPartConsecutiveSibling)
+        for r in range(offset_arcs, offset_arcs + num_arcs):
+            part = parts[r]
+            v = graph.create_binary_variable(part)
+            arc_hm = parts.find_arc_index(part.head, part.modifier)
+            arc_hs = parts.find_arc_index(part.head, part.sibling)
+
         graph.declare_factor(tree_factor, variables)
         tree_factor.initialize(length, arc_indices)
         graph.set_eta_ad3(.05)
@@ -34,11 +53,6 @@ class DependencyDecoder(StructuredDecoder):
 
         value, posteriors, additional_posteriors, status = \
             graph.solve_lp_map_ad3()
-        #print(status)
-        #print(value)
-        #predicted_arcs = \
-        #    [index for index, posterior in zip(arc_indices, posteriors)
-        #     if posterior > 0.1]
-        #print(predicted_arcs)
+
         predicted_output = posteriors
         return predicted_output
