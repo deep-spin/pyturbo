435c34e973e386651ee6377601e1e7ee56605ac9
Erick Rocha Fonseca <erickrfonseca@gmail.com>
2018-11-21 11:44:14 +0000

    update log analyzer to show UAS

--- scripts/analyze-logs.py
+++ scripts/analyze-logs.py
@@ -10,9 +10,12 @@ Script to analyze logs produced by the Turbo Parser
 def extract_data(filename):
     """
     Extract the best validation loss from the log file
+
+    :return: tuple (config dictionary, best UAS, loss at best UAS)
     """
     expecting_best_value = False
     loss = None
+    uas = None
 
     with open(filename, 'r') as f:
         config_line = next(f)
@@ -27,23 +30,29 @@ def extract_data(filename):
             elif 'Validation Loss' in line and expecting_best_value:
                 match = re.search('Validation Loss: ([\d.]+)\s', line)
                 loss = float(match.group(1))
+
+                match = re.search('validation UAS: ([\d.]+)\s', line)
+                uas = float(match.group(1))
                 expecting_best_value = False
 
-    return config_data, loss
+    return config_data, uas, loss
 
 
-def create_output_line(config_data, loss):
+def create_output_line(config_data, uas, loss):
     """
     Write a TSV line for the output with the model configuration and loss
     """
-    fields = [config_data['embedding_size'],
-              config_data['tag_embedding_size'],
-              config_data['learning_rate'],
-              config_data['rnn_size'],
-              config_data['mlp_size'],
-              config_data['dropout'],
-              config_data['num_layers'],
-              loss]
+    fields = []
+    if config_data is not None:
+        fields = [config_data['embedding_size'],
+                  config_data['tag_embedding_size'],
+                  config_data['learning_rate'],
+                  config_data['rnn_size'],
+                  config_data['mlp_size'],
+                  config_data['dropout'],
+                  config_data['num_layers']]
+    fields.append(uas)
+    fields.append(loss)
     line = '\t'.join(str(field) for field in fields)
     return line
 
@@ -51,9 +60,14 @@ def create_output_line(config_data, loss):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('logs', help='Log files', nargs='+')
+    parser.add_argument('--config', action='store_true',
+                        help='Show model configuration')
     args = parser.parse_args()
 
     output_lines = []
     for name in args.logs:
-        config, loss = extract_data(name)
-        print(create_output_line(config, loss))
+        config, dev_uas, loss = extract_data(name)
+        if not args.config:
+            config = None
+
+        print(create_output_line(config, dev_uas, loss))
