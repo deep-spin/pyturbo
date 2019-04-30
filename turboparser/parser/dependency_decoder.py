fixed loss computation
computing loss separately for each target

simplified some of the code

turboparser/parser/dependency_decoder.py
@@ -224,35 +224,6 @@ class DependencyDecoder(StructuredDecoder):
 
         return new_mask
 
-    def _get_margin(self, parts):
-        """
-        Compute and return a margin vector to be used in the loss and a
-        normalization term to be added to it.
-
-        It only affects Arcs or LabeledArcs (in case the latter are used).
-
-        :param parts: DependencyParts object
-        :type parts: DependencyParts
-        :return: a margin array to be added to the model scores and a
-            normalization constant
-        """
-        p = np.zeros(len(parts), dtype=np.float)
-        if parts.labeled:
-            # place the margin on LabeledArcs scores
-            # their offset in the gold vector is immediately after Arcs
-            offset = parts.num_arcs
-            num_parts = parts.num_labeled_arcs
-        else:
-            # place the margin on Arc scores
-            offset = 0
-            num_parts = parts.num_arcs
-
-        gold_values = parts.gold_parts[offset:offset + num_parts]
-        p[offset:offset + num_parts] = 0.5 - gold_values
-        q = 0.5 * gold_values.sum()
-
-        return p, q
-
     def _add_cost_vector(self, parts, scores):
         """
         Add the cost margin to the scores.
@@ -604,16 +575,7 @@ def make_score_matrix(length, arc_mask, scores):
     :return: a 2d numpy array (m, h), starting from 0
     """
     score_matrix = np.full([length, length], -np.inf, np.float32)
-
-    i = 0
     score_matrix[arc_mask] = scores
-    # for h in range(length):
-    #     for m in range(length - 1):
-    #         if not arc_mask[m, h]:
-    #             continue
-    #
-    #         score_matrix[m + 1, h] = scores[i]
-    #         i += 1
 
     return score_matrix.T
 
@@ -756,3 +718,4 @@ def tarjan(heads):
             strong_connect(i)
 
     return cycles
+

turboparser/parser/dependency_scorer.py
@@ -14,75 +14,61 @@ class DependencyNeuralScorer(object):
         self.part_scores = None
         self.model = None
 
-    def compute_gradients(self, instance_data, predicted_parts):
+    def compute_loss(self, instance_data, predicted_parts):
         """
-        Compute the error gradients for parsing and tagging.
+        Compute the losses for parsing and tagging.
 
         :param instance_data: InstanceData object
-        :param predicted_parts: same as gold_output
+        :param predicted_parts: list of numpy arrays with predicted parts for
+            each instance
+        :return: dictionary mapping each target to a loss scalar, as a torch
+            variable
         """
-        def _compute_loss(target_gold, logits):
-            targets = pad_labels(target_gold)
-
-            # cross_entropy expects (batch, n_classes, ...)
-            logits = logits.transpose(1, 2)
-            cross_entropy = F.cross_entropy(logits, targets, ignore_index=-1)
-
-            return cross_entropy
-
-        loss = torch.tensor(0.)
+        losses = {}
         gold_labels = instance_data.gold_labels
-        if torch.cuda.is_available():
-            loss = loss.cuda()
 
         for target in [Target.UPOS, Target.XPOS, Target.MORPH]:
             if target not in gold_labels[0]:
                 continue
 
             target_gold = [item[target] for item in gold_labels]
+            padded_gold = pad_labels(target_gold)
             logits = self.model.scores[target]
-            loss += _compute_loss(target_gold, logits)
+
+            # cross_entropy expects (batch, n_classes, ...)
+            logits = logits.transpose(1, 2)
+            losses[target] = F.cross_entropy(logits, padded_gold,
+                                             ignore_index=-1)
+
+        # dependency parts loss
+        parts_loss = torch.tensor(0.)
+        if torch.cuda.is_available():
+            parts_loss = parts_loss.cuda()
 
         batch_size = len(instance_data)
         for i in range(batch_size):
             inst_parts = instance_data.parts[i]
             gold_parts = inst_parts.gold_parts
-            pred_item = predicted_parts[i]
+            inst_pred = predicted_parts[i]
             part_score_list = [self.model.scores[type_][i]
                                for type_ in inst_parts.type_order]
             part_scores = torch.cat(part_score_list)
-            diff = torch.tensor(pred_item - gold_parts, dtype=part_scores.dtype,
+            diff = torch.tensor(inst_pred - gold_parts, dtype=part_scores.dtype,
                                 device=part_scores.device)
-            parts_loss_i = torch.dot(part_scores, diff)
+            error = torch.dot(part_scores, diff)
+            margin, normalizer = inst_parts.get_margin()
+            inst_parts_loss = margin.dot(inst_pred) + normalizer + error
 
-            if parts_loss_i > 0:
-                loss += parts_loss_i
+            if inst_parts_loss > 0:
+                parts_loss += inst_parts_loss
             else:
-                if parts_loss_i < -10e-6:
+                if inst_parts_loss < -10e-6:
                     logging.warning(
-                        'Ignoring negative loss: %.6f' % parts_loss_i.item())
+                        'Ignoring negative loss: %.6f' % inst_parts_loss.item())
 
-        # Backpropagate to accumulate gradients.
-        if loss > 0:
-            loss.backward()
+        losses[target.DEPENDENCY_PARTS] = parts_loss / batch_size
 
-        return loss.item()
-
-    def compute_tag_loss(self, scores, gold_output, reduction='mean'):
-        """Compute the loss for any tagging subtask
-
-        scores and gold_output may be either a batch or a single instance.
-        """
-        if isinstance(gold_output[0], list):
-            gold_output = self.pad_labels(gold_output)
-            loss = F.cross_entropy(scores, gold_output, ignore_index=-1,
-                                   reduction=reduction)
-        else:
-            gold_output = torch.tensor(gold_output, dtype=torch.long)
-            scores = torch.tensor(scores[:len(gold_output)])
-            loss = F.cross_entropy(scores, gold_output, reduction=reduction)
-
-        return loss
+        return losses
 
     def compute_scores(self, instances, parts):
         """
@@ -147,7 +133,13 @@ class DependencyNeuralScorer(object):
         """
         self.scheduler.step(accuracy)
 
-    def make_gradient_step(self):
+    def make_gradient_step(self, losses):
+        """
+        :param losses: dictionary mapping targets to losses
+        """
+        loss = sum(losses.values())
+        loss.backward()
+
         self.optimizer.step()
         # Clear out the gradients before the next batch.
         self.model.zero_grad()

turboparser/parser/turbo_parser.py
@@ -3,7 +3,7 @@
 from ..classifier import utils
 from ..classifier.instance import InstanceData
 from .token_dictionary import TokenDictionary
-from .constants import Target
+from .constants import Target, target2string
 from .dependency_reader import read_instances
 from .dependency_writer import DependencyWriter
 from .dependency_decoder import DependencyDecoder, chu_liu_edmonds, \
@@ -14,6 +14,7 @@ from .dependency_scorer import DependencyNeuralScorer
 from .dependency_instance_numeric import DependencyInstanceNumeric
 
 import sys
+from collections import defaultdict
 import pickle
 import numpy as np
 import logging
@@ -224,42 +225,6 @@ class TurboParser(object):
         self.total_tokens = 0
         self.validation_uas = 0.
         self.validation_las = 0.
-
-    def _get_task_train_report(self):
-        """
-        Return task-specific metrics on training data.
-
-        :return: a string describing naive UAS on training data
-        """
-        uas = self.accumulated_uas / self.total_tokens
-        msg = 'Naive train UAS: %f' % uas
-        if not self.options.unlabeled:
-            las = self.accumulated_las / self.total_tokens
-            msg += '\tNaive train LAS: %f' % las
-
-        for target in self.additional_targets:
-            acc = self.accumulated_hits[target] / self.total_tokens
-            msg += '\t%s train acc: %f' % (target, acc)
-
-        return msg
-
-    def _get_task_valid_report(self):
-        """
-        Return task-specific metrics on validation data.
-
-        :return: a string describing naive UAS on validation data
-        """
-        msg = 'Naive validation UAS: %f' % self.validation_uas
-        if not self.options.unlabeled:
-            msg += '\tNaive validation LAS: %f' % self.validation_las
-        if self.options.predict_upos:
-            msg += '\tUPOS accuracy: %f' % self.validation_upos
-        if self.options.predict_xpos:
-            msg += '\tXPOS accuracy: %f' % self.validation_xpos
-        if self.options.predict_morph:
-            msg += '\tUFeats accuracy: %f' % self.validation_morph
-
-        return msg
 
     def _get_post_train_report(self):
         """
@@ -578,15 +543,10 @@ class TurboParser(object):
 
         self.validation_uas = accumulated_uas / total_tokens
         self.validation_las = accumulated_las / total_tokens
-        if Target.UPOS in self.additional_targets:
-            self.validation_upos = accumulated_tag_hits[
+        self.validation_accuracies = {}
+        for target in self.additional_targets:
+            self.validation_accuracies[target] = accumulated_tag_hits[
                                        Target.UPOS] / total_tokens
-        if Target.XPOS in self.additional_targets:
-            self.validation_xpos = accumulated_tag_hits[
-                                       Target.XPOS] / total_tokens
-        if Target.MORPH in self.additional_targets:
-            self.validation_morph = accumulated_tag_hits[
-                                        Target.MORPH] / total_tokens
 
         # always update UAS; use it as a criterion for saving if no LAS
         if self.validation_uas > self.best_validation_uas:
@@ -783,7 +743,9 @@ class TurboParser(object):
         self.time_gradient = 0
         start = time.time()
 
-        self.total_loss = 0.
+        self.total_losses = {target: 0. for target in self.additional_targets}
+        self.total_losses[Target.DEPENDENCY_PARTS] = 0.
+
         self._reset_task_metrics()
 
         if epoch == 0:
@@ -803,6 +765,7 @@ class TurboParser(object):
             batch_index = next_batch_index
 
         end = time.time()
+        time_train = end - start
 
         valid_start = time.time()
         self.neural_scorer.eval_mode()
@@ -811,26 +774,72 @@ class TurboParser(object):
         self._get_validation_metrics(valid_data, valid_pred)
         valid_end = time.time()
         time_validation = valid_end - valid_start
-        train_loss = self.total_loss / len(train_data)
-        validation_loss = np.array(valid_losses).mean()
 
-        logging.info('Time: %f' % (end - start))
-        logging.info('Time to score: %f' % self.time_scores)
-        logging.info('Time to decode: %f' % self.time_decoding)
-        logging.info('Time to do gradient step: %f' % self.time_gradient)
-        logging.info('Time to run on validation: %f' % time_validation)
+        self._epoch_report(time_train, time_validation, self.total_losses,
+                           valid_losses, len(train_data), len(valid_data))
+
         if self._should_save:
             self.save()
-            logging.info('Saved model')
             self.num_bad_epochs = 0
         else:
             self.num_bad_epochs += 1
 
-        logging.info('\t'.join(['Total Train Loss (cost augmented): %f'
-                                % train_loss,
-                                self._get_task_train_report(),
-                                'Validation Loss: %f' % validation_loss,
-                                self._get_task_valid_report()]))
+    def _epoch_report(self, train_time, validation_time, train_losses,
+                      valid_losses, train_size, valid_size):
+        """
+        Log a report of the training for an epoch.
+
+        :param train_losses: dictionary mapping targets to loss scalar values,
+            not normalized by number of instances
+        :param valid_losses: same as train_losses
+        """
+        logging.info('Training time: %f' % train_time)
+        logging.info('Time to score: %f' % self.time_scores)
+        logging.info('Time to decode: %f' % self.time_decoding)
+        logging.info('Time to do gradient step: %f' % self.time_gradient)
+        logging.info('Time to run on validation: %f' % validation_time)
+
+        def make_loss_msgs(losses, dataset_size):
+            msgs = []
+            for target in losses:
+                target_name = target2string[target]
+                normalized_loss = losses[target] / dataset_size
+                msg = '%s: %.4f' % (target_name, normalized_loss)
+                msgs.append(msg)
+            return msgs
+
+        msgs = ['Train losses:'] + make_loss_msgs(train_losses, train_size)
+        logging.info('\t'.join(msgs))
+
+        uas = self.accumulated_uas / self.total_tokens
+        msgs = ['Train accuracies:\tUAS: %.6f' % uas]
+        if not self.options.unlabeled:
+            las = self.accumulated_las / self.total_tokens
+            msgs.append('LAS: %.6f' % las)
+
+        for target in self.additional_targets:
+            target_name = target2string[target]
+            acc = self.accumulated_hits[target] / self.total_tokens
+            msgs.append('%s: %.6f' % (target_name, acc))
+        logging.info('\t'.join(msgs))
+
+        msgs = ['Validation losses:'] + make_loss_msgs(valid_losses,
+                                                       valid_size)
+        logging.info('\t'.join(msgs))
+
+        msgs = ['Validation accuracies:\tUAS: %.6f' % self.validation_uas]
+        if not self.options.unlabeled:
+            msgs.append('LAS: %.6f' % self.validation_las)
+        for target in self.additional_targets:
+            target_name = target2string[target]
+            acc = self.validation_accuracies[target]
+            msgs.append('%s: %.6f' % (target_name, acc))
+        logging.info('\t'.join(msgs))
+
+        if self._should_save:
+            logging.info('Saved model')
+
+        logging.info('\n')
 
     def _run_batches(self, instance_data, batch_size, return_loss=False):
         """
@@ -844,11 +853,11 @@ class TurboParser(object):
         :param return_loss: if True, include the losses in the return. This
             can only be True for data which have known gold output.
         :return: a list of predictions. If return_loss is True, a tuple with
-            the list of predictions and the list of losses.
+            the list of predictions and the dictionary of losses.
         """
         batch_index = 0
         predictions = []
-        losses = []
+        losses = defaultdict(float)
 
         while batch_index < len(instance_data):
             next_index = batch_index + batch_size
@@ -856,7 +865,11 @@ class TurboParser(object):
             result = self.run_batch(batch_data, return_loss)
             if return_loss:
                 batch_predictions = result[0]
-                losses.extend(result[1])
+                batch_losses = result[1]
+                batch_size = len(batch_data)
+                for target in batch_losses:
+                    # store non-normalized losses
+                    losses[target] += batch_size * batch_losses[target].item()
             else:
                 batch_predictions = result
 
@@ -885,6 +898,7 @@ class TurboParser(object):
                                                    instance_data.parts)
 
         predictions = []
+        all_predicted_parts = []
         for i in range(len(instance_data)):
             instance = instance_data.instances[i]
             parts = instance_data.parts[i]
@@ -892,6 +906,8 @@ class TurboParser(object):
 
             predicted_parts = self.decoder.decode(instance, parts, inst_scores)
             inst_prediction = {Target.DEPENDENCY_PARTS: predicted_parts}
+            if return_loss:
+                all_predicted_parts.append(predicted_parts)
             for target in self.additional_targets:
                 model_answer = inst_scores[target].argmax(-1)
                 inst_prediction[target] = model_answer
@@ -899,38 +915,11 @@ class TurboParser(object):
             predictions.append(inst_prediction)
 
         if return_loss:
-            losses = self.compute_loss_batch(instance_data, predictions, scores)
+            losses = self.neural_scorer.compute_loss(instance_data,
+                                                     all_predicted_parts)
             return predictions, losses
 
         return predictions
-
-    def compute_loss_batch(self, instance_data, predictions, scores):
-        """
-        Compute the loss for a batch of predicted parts and label scores.
-
-        :param scores: a list of dictionaries mapping target names to scores
-        """
-        gold_labels = instance_data.gold_labels
-        losses = np.zeros(len(instance_data), np.float)
-
-        for i, inst_predictions in enumerate(predictions):
-            inst_parts = instance_data.parts[i]
-            inst_gold_labels = gold_labels[i]
-            inst_scores = scores[i]
-            inst_predicted_parts = inst_predictions[Target.DEPENDENCY_PARTS]
-            parser_loss = self.decoder.compute_loss(
-                inst_parts, inst_predicted_parts, inst_scores)
-            losses[i] = parser_loss
-
-            for target in self.additional_targets:
-                target_scores = inst_scores[target]
-                gold_labels_target = inst_gold_labels[target]
-
-                target_losses = self.neural_scorer.compute_tag_loss(
-                    target_scores, gold_labels_target)
-                losses += np.array(target_losses)
-
-        return losses
 
     def train_batch(self, instance_data):
         '''
@@ -964,30 +953,16 @@ class TurboParser(object):
 
         # run the gradient step for the whole batch
         start_time = time.time()
-        loss = self.make_gradient_step(instance_data, all_predicted_parts)
-
-        # Update the total loss and cost.
-        if loss < 0.0:
-            if loss < -1e-6:
-                # len 2 means root + one word
-                msg_len_1 = '(sentence length 1)' \
-                    if len(instance) == 2 else ''
-                msg = 'Negative loss set to zero: %f %s' % (loss, msg_len_1)
-                logging.warning(msg)
-            loss = 0.0
-
-        self.total_loss += loss
+        losses = self.neural_scorer.compute_loss(instance_data,
+                                                 all_predicted_parts)
+        self.neural_scorer.make_gradient_step(losses)
+        batch_size = len(instance_data)
+        for target in losses:
+            # store non-normalized losses
+            self.total_losses[target] += batch_size * losses[target].item()
 
         end_time = time.time()
         self.time_gradient += end_time - start_time
-
-    def make_gradient_step(self, instance_data, pred_parts):
-        """
-        Perform a gradient step and return the loss
-        """
-        loss = self.neural_scorer.compute_gradients(instance_data, pred_parts)
-        self.neural_scorer.make_gradient_step()
-        return loss
 
     def decode_train(self, instance, parts, scores):
         """