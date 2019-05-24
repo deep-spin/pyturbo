4bd212ec6b593e4c7dc8bf2a1e9c81344c06e4c3
Erick Fonseca <erickrfonseca@gmail.com>
2019-05-24 14:30:15 +0100

    more plotting functionality

--- scripts/plot_from_log.py
+++ scripts/plot_from_log.py
@@ -38,18 +38,22 @@ if __name__ == '__main__':
     parser.add_argument('output', help='File to print log')
     parser.add_argument('-y', help='Y limits to show (2 values)',
                         nargs=2, default=[0, 1], type=float)
+    parser.add_argument('--names', nargs='+',
+                        help='Names of the plots. If not given, filenames will '
+                             'be used')
     args = parser.parse_args()
 
     metric = 'UAS'
+    names = args.logs if args.names is None else args.names
 
     all_mins = []
     all_maxs = []
     fig, ax = pl.subplots()
 
-    for log in args.logs:
+    for log, name in zip(args.logs, names):
         accuracies = extract_accuracies(log, metric)
         steps = np.arange(1, len(accuracies) + 1)
-        ax.plot(steps, accuracies)
+        ax.plot(steps, accuracies, label=name)
 
         all_maxs.append(accuracies.max())
         all_mins.append(accuracies.min())
@@ -58,6 +62,7 @@ if __name__ == '__main__':
     y_max = min(args.y[1], 1.04 * max(all_maxs))
 
     ax.set_yticks(all_maxs, minor=True)
+    ax.legend()
     pl.xlabel('Evaluation step')
     pl.ylabel(metric)
     pl.ylim(y_min, y_max)
