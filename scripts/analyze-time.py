89c09b38fcdc4cb24f4adc163378a927b883588d
Erick Rocha Fonseca <erickrfonseca@gmail.com>
2019-09-16 16:38:36 +0100

    improved script for analyzing logs

--- scripts/analyze-time.py
+++ scripts/analyze-time.py
@@ -8,9 +8,28 @@ import numpy as np
 Analyze how much time batches took in log files.
 """
 
+
+def format_time(num_seconds):
+    s = ''
+    if num_seconds > 60:
+        num_minutes = num_seconds // 60
+        num_seconds = num_seconds % 60
+        if num_minutes > 60:
+            num_hours = num_minutes // 60
+            num_minutes = num_minutes % 60
+            s += '%dh ' % num_hours
+        s += '%dm ' % num_minutes
+    s += '%.2fs' % num_seconds
+
+    return s
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('log', help='Log file')
+    parser.add_argument('--mean', action='store_true', help='Show mean times')
+    parser.add_argument('--detailed', action='store_true',
+                        help='Show time for scoring/gradient/decoding')
     args = parser.parse_args()
 
     with open(args.log, 'r') as f:
@@ -37,18 +56,25 @@ if __name__ == '__main__':
     total_scoring = time_scoring.sum()
     total_decoding = time_decoding.sum()
     total_gradient = time_gradient.sum()
+    total_scoring_str = format_time(total_scoring)
+    total_decoding_str = format_time(total_decoding)
+    total_gradient_str = format_time(total_gradient)
 
     print('%d batches run in total' % num_batches)
-    print('Total time spent in scoring: %.2fs' % total_scoring)
-    print('Total time spent in decoding: %.2fs' % total_decoding)
-    print('Total time spent in weight gradients: %.2fs' % total_gradient)
-    print('Total time: %.2fs\n' % times.sum())
+    if args.detailed:
+        print('Total time spent in scoring: %s' % total_scoring_str)
+        print('Total time spent in decoding: %s' % total_decoding_str)
+        print('Total time spent in weight gradients: %s' % total_gradient_str)
+    print('Total time: %s\n' % format_time(times.sum()))
 
-    mean_scoring = total_scoring / num_batches
-    mean_decoding = total_decoding / num_batches
-    mean_gradient = total_gradient / num_batches
+    mean_scoring = format_time(total_scoring / num_batches)
+    mean_decoding = format_time(total_decoding / num_batches)
+    mean_gradient = format_time(total_gradient / num_batches)
+    mean_total = format_time(times.sum() / num_batches)
 
-    print('Mean scoring time per batch: %.2fs' % mean_scoring)
-    print('Mean decoding time per batch: %.2fs' % mean_decoding)
-    print('Mean gradient time per batch: %.2fs' % mean_gradient)
-    print('Mean time per batch: %.2fs' % (times.sum() / num_batches))
+    if args.mean:
+        if args.detailed:
+            print('Mean scoring time per batch: %s' % mean_scoring)
+            print('Mean decoding time per batch: %s' % mean_decoding)
+            print('Mean gradient time per batch: %s' % mean_gradient)
+        print('Mean time per batch: %s' % mean_total)
